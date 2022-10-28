59852f4bb7378671d75ebcba5c871580174a8e10
Gary Montero <gmm413@gmail.com>
2022-10-28 15:59:42 -0400

    Lecture 39 & 40: Input Search Box Component and Searching & Filtering

--- src/App.js
+++ src/App.js
@@ -33,6 +33,21 @@ class App extends Component {
     console.log("render");
     return (
       <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="Search Monsters"
+          onChange={(event) => {
+            console.log(event.target.value);
+            const searchString = event.target.value.toLocaleLowerCase();
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(searchString);
+            });
+            this.setState(() => {
+              return { monsters: filteredMonsters };
+            });
+          }}
+        />
         {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
