d870956b0c30cd0cf617a701bde726be85bb26f5
Gary Montero <gmm413@gmail.com>
2022-11-01 16:36:39 -0400

    Lecture 60: Monsters Rolodex - Hooks: useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import "./App.css";
 import Cardlist from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 
 const App = () => {
+  console.log("render");
   const [searchField, setSearchField] = useState("");
-  console.log(searchField);
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+  useEffect(() => {
+    console.log("effect fired");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  }, []);
+
+  useEffect(() => {
+    const newfilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    setFilteredMonsters(newfilteredMonsters);
+    console.log("efffect is firing");
+  }, [monsters, searchField]);
 
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLocaleLowerCase();
@@ -21,7 +39,7 @@ const App = () => {
         placeholder="Search Monsters"
         className="monsters-search-box"
       />
-      {/* {<Cardlist monsters={filteredMonsters} /> } */}
+      {<Cardlist monsters={filteredMonsters} />}
     </div>
   );
 };
